818c91a098e155ce7fa3b8f6118ca477422488cd
박준현 <gktkwk91@naver.com>
2024-05-10 10:01:44 +0900

    apply #1 스프링부트 적용
    스프링부트  initializer를 사용하여 지난 시간에 활용했던 정렬 구현을 가지고와서 적용 완료
    https://github.com/parkkkkjuneHyeon/java-oop-practice.git

---
+++ spring-boot-practice/src/main/java/com/fast/logic/BubbleSort.java
@@ -0,0 +1,27 @@
+package com.fast.logic;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class BubbleSort <T extends Comparable<T>> implements Sort<T>{
+
+    @Override
+    public List<T> sort(List<T> list) {
+        List<T> output = new ArrayList<>(list);
+        int listLen = output.size();
+
+        for(int i=listLen-1; i>0; i--) {
+            for(int j=0; j<i; j++) {
+                if(output.get(j).compareTo(output.get(j+1)) > 0) {
+                    T temp = output.get(j);
+                    output.set(j, output.get(j+1));
+                    output.set(j+1, temp);
+                }
+            }
+        }
+        return output;
+    }
+}
--- spring-boot-practice/src/test/java/com/fast/springbootpractice/SpringBootPracticeApplicationTests.java
+++ spring-boot-practice/src/test/java/com/fast/springbootpractice/SpringBootPracticeApplicationTests.java
@@ -3,11 +3,15 @@ package com.fast.springbootpractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+//스프링 부트를 테스트 프로파일에서
+// 애플리케이션 컨텍스트를 읽어와서 사용할수 있게끔 준비해줌
 @SpringBootTest
 class SpringBootPracticeApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
