fd8f753cc931dc85802876d902890df6ad3eedf2
박준현 <gktkwk91@naver.com>
2024-05-10 19:15:15 +0900

    apply #6 롬북 적용
    롬북 어노테이션들을 이용해서 필요한 부분만 적용

--- spring-boot-practice/src/main/java/com/fast/controller/MainController.java
+++ spring-boot-practice/src/main/java/com/fast/controller/MainController.java
@@ -2,20 +2,22 @@ package com.fast.controller;
 
 
 import com.fast.service.SortService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RequiredArgsConstructor
 @RestController
 public class MainController {
 
     private final SortService sortService;
 
-    public MainController(SortService sortService) {
-        this.sortService = sortService;
-    }
+//    public MainController(SortService sortService) {
+//        this.sortService = sortService;
+//    }
 
     @GetMapping("/")
     public String hello(@RequestParam List<String> list) {
